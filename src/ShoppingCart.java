34e23c1afa28b69abf62f064de21936b34033d67
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-10-22 18:33:15 +0300

    Ex21

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -5,12 +5,12 @@ public class ShoppingCart {
         this.products = new Product[10];
     }
 
+
     public void calculateTotal() {
         double sum = 0;
         for (Product p : products) {
             sum = sum + p.getPrice();
         }
-
         System.out.println("The total price is: " + sum);
     }
 }
